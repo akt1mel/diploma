881b7014db766ce4f34d3609ce02591823bb278c
akt1mel <kostyavas.94@mail.ru>
2018-10-22 23:21:00 +0300

    Добавлено экранирование, обновлен основной язык.

--- views/site/index.php
+++ views/site/index.php
@@ -9,7 +9,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">
                     <h4 class="panel-title">
-                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#<?= $question['id'] ?>"><?php  echo $question['description']; ?></a>
+                        <a class="accordion-toggle" data-toggle="collapse" data-parent="#accordion" href="#<?= Html::encode($question['id']) ?>"><?= Html::encode($question['description']) ?></a>
                     </h4>
                 </div>
                 <div id="<?= Html::encode($question['id']) ?>" class="panel-collapse collapse in">
